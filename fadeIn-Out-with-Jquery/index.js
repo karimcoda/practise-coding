22d095a5be740876091f1dabef188220be32c305
karimcoda <40443372+karimcoda@users.noreply.github.com>
2020-03-30 22:22:23 +0200

    Create index.js

---
+++ fadeIn-Out-with-Jquery/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#button-1').click(function(){
+        $('#num-1').fadeToggle();
+    });
+    $('#button-2').click(function(){
+        $('#num-2').fadeToggle();
+    });
+});
