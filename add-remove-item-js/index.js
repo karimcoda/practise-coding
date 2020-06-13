0aeab759459c4634ebc79ec7964a238287541250
karimcoda <40443372+karimcoda@users.noreply.github.com>
2020-06-14 01:07:15 +0200

    Update index.js

--- add-remove-item-js/index.js
+++ add-remove-item-js/index.js
@@ -1,38 +1,55 @@
-var inputHeadingText = document.querySelector('#inputH').value,
-    inputText = document.querySelector('#inputP').value,
+var inputHeadingText = document.querySelector('#inputH'),
+    inputText = document.querySelector('#inputP'),
     itemsList = document.querySelector('.items'),
     addItem = document.querySelector('#add'),
     removeItem = document.querySelector('#remove'),
     selectAll = document.querySelector('.all');
 
+    
+
+
     function addNewItem() {
-        const   createitem = document.createElement('div'),
-                createTextH = document.createTextNode(inputHeadingText),
-                createTextP = document.createTextNode(inputText),
-                createDivContent = document.createElement('div'),
-                createbtn = document.createElement('a'), 
-                createHeading = document.createElement('h2'),
-                createPara = document.createElement('p'),
-                createTextBtn = document.createTextNode('X');
+        if (inputHeadingText.value == '' ){
+            inputHeadingText.setAttribute('placeholder', 'Enter your Name');
+            inputText.setAttribute('placeholder', 'Enter Your Career');
+    
+    
+            
+            
+        }else{
+            
+            const   
+                    inputHeadingTextValue = inputHeadingText.value,
+                    inputTextValue = inputText.value,
+                    createitem = document.createElement('div'),
+                    createTextH = document.createTextNode(inputHeadingTextValue),
+                    createTextP = document.createTextNode(inputTextValue),
+                    createDivContent = document.createElement('div'),
+                    createbtn = document.createElement('a'), 
+                    createHeading = document.createElement('h2'),
+                    createPara = document.createElement('p'),
+                    createTextBtn = document.createTextNode('X');
 
-        createitem.setAttribute('class', 'item');
-        createDivContent.setAttribute('class', 'text'),
-        createbtn.setAttribute('class', 'del');
+            createitem.setAttribute('class', 'item');
+            createDivContent.setAttribute('class', 'text'),
+            createbtn.setAttribute('class', 'del');
 
-        createbtn.appendChild(createTextBtn);
+            createbtn.appendChild(createTextBtn);
 
-        createHeading.appendChild(createTextH);
-        createPara.appendChild(createTextP);
+            createHeading.appendChild(createTextH);
+            createPara.appendChild(createTextP);
 
-        createDivContent.appendChild(createHeading);
-        createDivContent.appendChild(createPara);
+            createDivContent.appendChild(createHeading);
+            createDivContent.appendChild(createPara);
 
-        createitem.appendChild(createDivContent);
-        createitem.appendChild(createbtn); 
+            createitem.appendChild(createDivContent);
+            createitem.appendChild(createbtn); 
 
-        itemsList.appendChild(createitem);
+            itemsList.appendChild(createitem);
+            
+            createbtn.addEventListener('click', del_Item);
+        }
         
-        createbtn.addEventListener('click', del_Item);
 
     }
 
